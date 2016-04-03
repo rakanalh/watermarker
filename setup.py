1ab17c0029721bd9609e815be0d9415aea782165
Rakan Alhneiti <rakan.alhneiti@gmail.com>
2016-04-03 14:32:39 +0200

    Fork

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+
+setup(
+    name='watermarker',
+    version='1.1',
+    packages=['watermarker'],
+    install_requires=[
+        'pillow',
+    ],
+    author='Pavel Zhukov',
+    author_email='gelios@gmail.com',
+    description='Library for add text watermarks to images using PIL, '
+                'support sorl-thumbnail integration',
+    long_description=open('README').read(),
+    license='GPL',
+    keywords='watermark, library',
+    url='http://bitbucket.org/zeus/watermarker'
+)
